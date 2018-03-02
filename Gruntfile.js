fd89ea190736652cf4043233da72673263422068
George Bochenek <bochenekgeorge@gmail.com>
2018-03-02 10:01:24 -0500

    WAB Widget Shell From the Generator

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadNpmTasks('grunt-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-sass');
+    var appDir = 'C:/arcgis-web-appbuilder-2.7/WebAppBuilderForArcGIS/server/apps/4';
+    var stemappDir = 'C:/arcgis-web-appbuilder-2.7/WebAppBuilderForArcGIS/client/stemapp3d';
+    grunt.initConfig({
+        sync: {
+            main: {
+                verbose: true,
+                files: [
+                    {
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: stemappDir
+                    },
+                    {
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: appDir
+                    }
+                ]
+            }
+        },
+        babel: {
+            'main': {
+                'files': [{
+                        'expand': true,
+                        'src': [
+                            'widgets/*.js',
+                            'widgets/**/*.js',
+                            'widgets/**/**/*.js',
+                            'widgets/!**/**/nls/*.js',
+                            'themes/*.js',
+                            'themes/**/*.js',
+                            'themes/**/**/*.js',
+                            'themes/!**/**/nls/*.js'
+                        ],
+                        'dest': 'dist/'
+                    }]
+            }
+        },
+        watch: {
+            main: {
+                files: [
+                    'widgets/**',
+                    'themes/**'
+                ],
+                tasks: [
+                    'clean',
+                    'sass',
+                    'babel',
+                    'copy',
+                    'sync'
+                ],
+                options: {
+                    spawn: false,
+                    atBegin: true,
+                    livereload: true
+                }
+            }
+        },
+        copy: {
+            'main': {
+                'src': [
+                    'widgets/**/**.html',
+                    'widgets/**/**.json',
+                    'widgets/**/**.css',
+                    'widgets/**/images/**',
+                    'widgets/**/nls/**',
+                    'themes/**/**.html',
+                    'themes/**/**.json',
+                    'themes/**/**.css',
+                    'themes/**/images/**',
+                    'themes/**/nls/**'
+                ],
+                'dest': 'dist/',
+                'expand': true
+            }
+        },
+        clean: { 'dist': { 'src': 'dist/**' } },
+        sass: {
+            dist: {
+                options: { sourceMap: true },
+                files: [{
+                        expand: true,
+                        src: ['widgets/**/*.scss'],
+                        rename: function (dest, src) {
+                            return src.replace('scss', 'css');
+                        }
+                    }]
+            }
+        }
+    });
+    grunt.registerTask('default', ['watch']);
+};
